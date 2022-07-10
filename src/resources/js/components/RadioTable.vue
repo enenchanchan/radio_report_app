d99c0d5ea78c47c647573b8793e6c00cb10ac1f4
enenchanchan <20enchange22@gmail.com>
2022-07-10 14:50:28 +0900

    検索機能実装#35

--- src/resources/js/components/RadioTable.vue
+++ src/resources/js/components/RadioTable.vue
@@ -1,21 +1,20 @@
 <template>
 <div>
     <h2>Vue版</h2>
-    <div> {{message}}:<input type="text" v-model="search"></div>
-<h1>{{sort_key}}</h1>
+    <div > {{message}}:<input type="text" v-model="search" class="w-75"></div>
 <table class="table">
     <thead>
         <tr>
-            <th @click="sortBy('radio_title')">番組名</th>
-            <th @click="sortBy('radio_date')">放送日時</th>
-            <th @click="sortBy('broadcaster')">放送局</th>
+            <th>番組名</th>
+            <th>放送日時</th>
+            <th>放送局</th>
         </tr>
     </thead>
     <tbody>
-        <tr v-for="radio in sort_radios" :key="radio.id">
-            <td >{{radio.radio_title}}</td>
-            <td >{{radio.radio_date}}</td>
-            <td >{{radio.broadcaster}}</td>
+        <tr v-for="radio in search_radios" :key="radio.id">
+            <td><a :href="`/radios/${radio.id}`">{{radio.radio_title}}</a></td>
+            <td>{{radio.radio_date}}</td>
+            <td>{{radio.broadcaster}}</td>
         </tr>
     </tbody>
 </table>
@@ -35,8 +34,6 @@ return{
     message:"検索",
     radios:[],
     search:'',
-    sort_key:"",
-sort_asc:true,
 }
         },
         computed:{
@@ -46,29 +43,6 @@ sort_asc:true,
                     radio.broadcaster.includes(this.search)
                 })
             },
-            sort_radios(){
- if (this.sort_key != "") {
-     let set =1;
-     this.sort_asc?(set =1):(set=-1);
-      this.radios.sort((a, b) => {
-        if (a[this.sort_key] < b[this.sort_key]) return -1 * set;
-        if (a[this.sort_key] > b[this.sort_key]) return 1 * set ;
-        return 0;
-      });
-      return this.radios;
-    } else {
-      return this.radios;
-    }
-            },
         },
-        methods:{
-            sortBy(key){
-                this.sort_key === key
-                ? (this.sort_asc = !this.sort_asc)
-                :(this.sort_asc =true);
-                this.sort_key =key;
-            }
-
-        }
 }
 </script>
--- src/resources/views/radios/list.blade.php
+++ src/resources/views/radios/list.blade.php
@@ -2,23 +2,5 @@
 @section('title','登録番組一覧')
 @include('layouts.nav')
 @section('content')
-<table class="table">
-    <thead>
-        <tr>
-            <th scope="col">番組名</th>
-            <th scope="col">放送日時</th>
-            <th scope="col">放送局</th>
-        </tr>
-    </thead>
-    @foreach($radios as $radio)
-    <tbody>
-        <tr>
-            <th><a href="{{route('radios.show',['radio'=>$radio])}}">{{$radio->radio_title}}</a></th>
-            <th>{{$radio->radio_date}}</th>
-            <th>{{$radio->broadcaster}}</th>
-        </tr>
-    </tbody>
-    @endforeach
-</table>
 <radio-table :radios="{{$radios}}"></radio-table>
 @endsection
