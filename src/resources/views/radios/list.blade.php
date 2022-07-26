05e384c168d27fb8761935757e7625e0acd76ff1
enenchanchan <20enchange22@gmail.com>
2022-07-26 19:56:46 +0900

    番組リストページ修正#71

--- src/resources/views/radios/list.blade.php
+++ src/resources/views/radios/list.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.app')
 @section('title','登録番組一覧')
 @section('content')
-<h1 class="text-center m-3">登録番組一覧</h1>
-<radio-table :radios="{{$radios}}"></radio-table>
+<h1 class="text-center mt-5">登録番組一覧</h1>
+<radio-table :radios="{{$radios}}" class="mt-3"></radio-table>
 @endsection
