c11ac49b4c73cb898dcf8e52a446a497f2b932db
enenchanchan <20enchange22@gmail.com>
2022-08-24 22:06:46 +0900

    投稿検索再実装#95

--- src/resources/views/articles/index.blade.php
+++ src/resources/views/articles/index.blade.php
@@ -1,8 +1,19 @@
 @extends('layouts.app')
 @section('title','投稿一覧')
 @section('content')
+<div class="mt-5 text-center">
+    <form action="{{route('articles.index')}}" method="GET">
+        <input type="search" class="w-75" name="keyword" value="{{$keyword}}" placeholder="投稿を検索">
+        <button type="submit" class="btn btn-primary"><i class="fa-solid fa-magnifying-glass"></i></button>
+    </form>
+</div>
 @foreach($articles as $article)
 @include('articles.about')
 @endforeach
-{{$articles->links('vendor.pagination.bootstrap-5')}}
+@if(count($articles) == 0 )
+<div class="text-center text-danger mt-5">
+    <h2>条件に合致する投稿がありません。</h2>
+</div>
+@endif
+{{$articles->appends(request()->query())->links('vendor.pagination.bootstrap-5')}}
 @endsection
