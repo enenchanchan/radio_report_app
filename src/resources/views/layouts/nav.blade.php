b00519be809935e6546295825c68c215177b8055
enenchanchan <20enchange22@gmail.com>
2022-09-07 21:34:58 +0900

    レイアウト微調整#51

--- src/resources/views/layouts/nav.blade.php
+++ src/resources/views/layouts/nav.blade.php
@@ -19,8 +19,8 @@
                         <li class="nav-item"><a href="{{route('radios.index')}}" class="nav-link">登録番組一覧</a></li>
                         @endguest
                         @auth
-                        <li class="nav-item"><a href="{{route('articles.create')}}" class="nav-link">投稿する</a></li>
-                        <li class="nav-item"><a href="{{route('radios.create')}}" class="nav-link">番組の新規登録</a></li>
+                        <li class="nav-item"><a href="{{route('articles.create')}}" class="nav-link">聴取メモ投稿</a></li>
+                        <li class="nav-item"><a href="{{route('radios.create')}}" class="nav-link">番組新規登録</a></li>
                         <li class="nav-item"><a href="{{route('radios.index')}}" class="nav-link">登録番組一覧</a></li>
                         <li class="nav-item"><a href="{{route('users.show',['user' => Auth::user()->id])}}" class="nav-link">マイページ</a></li>
                         <li class="nav-item">
